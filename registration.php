155b38dceb0b8d76e7fa46c7c1e73b559e868002
d4rkd3vil <d4rkd3vil0005@gmail.com>
2023-07-10 23:26:17 +0530

    php test

--- registration.php
+++ registration.php
@@ -1,2 +1,21 @@
 <?php
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "root";
+    $dbname = "June_Natural_Soap_Store";
+
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $contry_code = $_POST['contry-code'];
+    $mobile_number = $_POST['mobile-number'];
+    $email = $_POST['email'];
+    $date_of_birth = $_POST['dob'];
+    $pass = $_POST['password'];
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if(conn->connect_error)
+    {
+        echo "connection successful";
+        $sql = "INSERT INTO TABLE user_credentials (first_name, last_name, contry_code, mobile_number, email, date_of_birth, password) VALUES ('$first_name', '$last_name, '$contry_code', '$mobile_number', '$email', '$date_of_birth', '$pass')";
+    }   
 ?>
