5f6256ad53c4e69f9cbfa9319ae4a8fc0cdcd950
root <root@atom.STELFIBER>
2023-08-20 23:20:08 +0530

    home page doing validation of registation and connectivity

--- home.php
+++ home.php
@@ -13,31 +13,84 @@
 </head>
 <body>
     <header>
-        <nav>
-            <div class="row container bar">
-                <div class="col-md-4">
-                    <h3><a href="login.html" class="title">JUNE</a></h3>
+        <nav class="navbar navbar-light bg-primary navbar-expand-lg">
+            <div class="container-fluid row">
+                <div class="col">
+                    <a class="title" href="#"><h5>JUNE</h5></a>
                 </div>
-                <div class="col-md-4">
-
+                <div class="col">
+                    <form class="d-flex">
+                        <input class="form-control me-2 search_bar search_input" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-dark search_btn" type="submit">Search</button>
+                    </form>
                 </div>
-                <div class="col-md-4">
-
+                <div class="col" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown m-3">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Log in
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Switch to another ac</a></li>
+                                <li><a class="dropdown-item" href="#">profile</a></li>
+                                <li><a class="dropdown-item" href="index.html">log out</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item m-3">Support</li>
+                        <li class="nav-item m-3">WishList</li>
+                        <li class="nav-item m-3">Cart</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><h5>JUNE</h5></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
     </header>
     <section>
         <div class="container-fluid">
-            <div class="">
-                <h2>hello</h2>
+            <div class="gallery-container">
+                <div class="gallery">
+                    <img src="soap/Snapchat-1914282164.jpg" alt="Image 1">
+                    <img src="soap/Snapchat-1669480039.jpg" alt="Image 2">
+                    <img src="soap/Snapchat-20742154.jpg" alt="Image 3">
+                </div>
             </div>
         </div>
     </section>
     <footer>
         <div class="container-fluid">
-            <div class="">
-                <h5>hello</h5>
+            <div class="feet">
+                <p>contact us</p>
             </div>
         </div>
     </footer>
