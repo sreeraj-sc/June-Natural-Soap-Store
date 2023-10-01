2596760aebd3a15493c8bd56711d14a5c107b7c3
root <root@atom.STELFIBER>
2023-10-02 01:36:30 +0530

    slide show should be currected. it is not working because the newly added script by bard is not working

--- home.php
+++ home.php
@@ -7,28 +7,99 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="style/home.css" type="text/css">
     <link rel="icon" href="images/June-logo-3.png" type="image/png" sizes="16x16">
+    <style>
+        * {box-sizing: border-box;}
+        body {font-family: Verdana, sans-serif;}
+        .mySlides {display: none;}
+        img {vertical-align: middle;}
+
+        /* Slideshow container */
+        .slideshow-container {
+          max-width: 1000px;
+          position: relative;
+          margin: auto;
+        }
+
+        /* Caption text */
+        .text {
+          color: #f2f2f2;
+          font-size: 15px;
+          padding: 8px 12px;
+          position: absolute;
+          bottom: 8px;
+          width: 100%;
+          text-align: center;
+        }
+
+        /* Number text (1/3 etc) */
+        .numbertext {
+          color: #f2f2f2;
+          font-size: 12px;
+          padding: 8px 12px;
+          position: absolute;
+          top: 0;
+        }
+
+        /* The dots/bullets/indicators */
+        .dot {
+          height: 15px;
+          width: 15px;
+          margin: 0 2px;
+          background-color: #bbb;
+          border-radius: 50%;
+          display: inline-block;
+          transition: background-color 0.6s ease;
+        }
+
+        .active {
+          background-color: #717171;
+        }
+
+        /* Fading animation */
+        .fade {
+          animation-name: fade;
+          animation-duration: 1.5s;
+        }
+
+        @keyframes fade {
+          from {opacity: .4} 
+          to {opacity: 1}
+        }
+
+        /* On smaller screens, decrease text size */
+        @media only screen and (max-width: 300px) {
+          .text {font-size: 11px}
+        }
+        .mySlides img 
+        {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+        }
+</style>
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container">
-                <a class="navbar-brand" href="#">My E-Commerce Site</a>
+                <a class="navbar-brand" href="#">June</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="">Home</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Shop
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Category 1</a></li>
-                                <li><a class="dropdown-item" href="#">Category 2</a></li>
-                                <li><a class="dropdown-item" href="#">Category 3</a></li>
+                                <li><a class="dropdown-item" href="">Category 1</a></li>
+                                <li><a class="dropdown-item" href="">Category 2</a></li>
+                                <li><a class="dropdown-item" href="">Category 3</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
@@ -40,23 +111,46 @@
                     </ul>
                 </div>
             </div>
-            <div class="search-bar fixed-top w-100">
-                <form class="d-flex">
-                    <input class="form-control w-100" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
         </nav>
+        <form class="d-flex search-bar justify-content-center align-items-center mt-1">
+            <input class="form-control w-50" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
     </header>
     <section>
+    <div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="slide_show/image1.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="slide_show/image2.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="slide_show/image3.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
 
     </section>
     <footer>
-
     </footer>
-</body>
-</html>
-
+    <script src="script/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> 
 </body>
 </html>
--- script/script.js
+++ script/script.js
@@ -22,3 +22,40 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+// Get the image elements
+const images = document.querySelectorAll("img");
+
+// Set the max-width of the images to 100%
+for (const image of images) {
+  image.style.maxWidth = "100%";
+  image.style.height = "auto";
+  image.style.display = "block";
+  image.style.margin = "0 auto";
+}
+
+// Set the slide index to 0
+let slideIndex = 0;
+
+// Show the slides
+showSlides();
+
+function showSlides() {
+  // Hide all the slides
+  for (const slide of slides) {
+    slide.style.display = "none";
+  }
+
+  // Show the current slide
+  slides[slideIndex].style.display = "block";
+
+  // Increment the slide index
+  slideIndex++;
+
+  // If the slide index is greater than the number of slides, reset it to 0
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+
+  // Call the showSlides() function again in 2 seconds
+  setTimeout(showSlides, 2000);
+}
