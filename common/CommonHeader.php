2f2c469b4352e51fdd572a6744df92ade2b72e86
root <root@DESKTOP-PK4P42O>
2023-10-27 21:01:34 +0530

    now all set in user

--- common/CommonHeader.php
+++ common/CommonHeader.php
@@ -81,6 +81,9 @@
               <li class="nav__item">
                 <a href="#contact" class="nav__link scroll-link">Contact</a>
               </li>
+              <li class="nav__item">
+                <a href="logout.php" class="nav__link scroll-link">log out</a>
+              </li>
             </ul>
           </div>
 
