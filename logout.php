3aba354e055d9f6c2b41d79bd7624f3883d23cb0
root <root@DESKTOP-PK4P42O>
2023-10-27 22:01:16 +0530

    another error solved

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: login.php");
+$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Unknown';
+header("Location: " . $referrer);
 exit;
 ?>
